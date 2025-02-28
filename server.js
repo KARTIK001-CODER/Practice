01b19ee24a2c8b3aed7bae6527d9c0b44072fe46
KARTIK001-CODER <0011kartikchoudhary@gmail.com>
2025-02-28 20:29:09 +0530

    Day 2

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const http = require("http");
-const port = 3000;
+// const http = require("http");
+const Port = 9000;
 
-const Server = http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type" : "Plain.Text"});
-    res.end("Hello Kartik , Welcome Back");
-})
+// const Server = http.createServer((req,res)=>{
+//     res.writeHead(200,{"Content-Type" : "Plain.Text"});
+//     res.end("Hello Kartik , Welcome Back");
+// })
 
 
-Server.listen(port , ()=>{
-    console.log(`Port is Running on ${port} `)
+// Server.listen(port , ()=>{
+//     console.log(`Port is Running on ${port} `)
+// })
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("Hello There, It good to see you Again!!!");
+});
+
+app.listen(Port , ()=>{
+    console.log(`Port is running on ${Port}`);
 })
